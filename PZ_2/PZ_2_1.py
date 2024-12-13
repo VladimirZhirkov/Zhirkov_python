4b73ff2115075e9df347fe17806fc0e31b20964c
Undertaker <106561978+VladimirZhirkov@users.noreply.github.com>
2024-12-13 23:58:43 +0300

    Add files via upload

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,1 +1,18 @@
-print("hello woreld")
+def main():
+    try:
+        A = int(input("Введите число A: "))
+        B = int(input("Введите число B: "))
+
+        if A <= 0 or B <= 0:
+            raise ValueError("Числа A и B должны быть положительными.")
+        if A <= B:
+            raise ValueError("A должно быть больше B.")
+
+        незанятая_часть = A % B
+        print("Длина незанятой части отрезка A:", незанятая_часть)
+
+    except ValueError as e:
+        print("Ошибка:", e)
+
+if __name__ == "__main__":
+    main()
