09d706b8852ac6fd2b9b777fd4cf3b8e0da99fcc
Undertaker <106561978+VladimirZhirkov@users.noreply.github.com>
2025-02-15 16:59:45 +0300

    Update PZ_3_1.py

--- PZ3/PZ_3_1.py
+++ PZ3/PZ_3_1.py
@@ -4,7 +4,7 @@ B = int(input("Введите целое число B: "))
 is_A_odd = A % 2 != 0
 is_B_odd = B % 2 != 0
 
-if is_A_odd ^ is_B_odd:  # Исключающее ИЛИ
+if is_A_odd ^ is_B_odd:
     print("Ровно одно из чисел A и B нечетное.")
 else:
     print("Либо оба числа четные, либо оба нечётные.")
